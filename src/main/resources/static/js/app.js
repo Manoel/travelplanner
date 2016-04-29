b51ee61de7ffe9af2c1cadc3f3150d3e4a010aef
Manoel Menezes <manoel.menezes.jr@gmail.com>
2016-04-29 04:20:27 -0300

    first commit

---
+++ src/main/resources/static/js/app.js
@@ -0,0 +1,10 @@
+(function() {
+	'use strict';
+
+	angular.module('travelplanner', []).controller('home', function($scope) {
+		$scope.greeting = {
+			id : 'xxx',
+			content : 'Hello World!'
+		}
+	})
+})();
